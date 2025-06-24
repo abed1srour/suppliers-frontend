e810d5574d50d4cc2a7d1327a1d304dfb71f35d7
abed1srour <abedallatifsrour@hotmail.com>
2025-06-24 09:34:27 +0300

    Force add all changes including footer

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,10 @@ const nextConfig = {
         protocol: '*',
         hostname: '**', // Allow all protocols and all domains
       },
+      {
+        protocol: 'https',
+        hostname: 'www.felicitysolar.com', // Explicitly allow felicitysolar.com
+      },
     ],
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
